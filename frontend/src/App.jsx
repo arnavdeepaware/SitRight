6b0d7065b4f3924653b0a0ce7a339d179059f9d8
Aditya Dwivedi <aditya.vd2002@gmail.com>
2025-03-07 17:50:31 -0500

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Nav from './nav.jsx'
-=======
 import React from "react";
 import "./App.css";
->>>>>>> a7b5a24 (Nothing)
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-     
-     <Nav />
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      
-    </>
-  )
-=======
     <div>
       <header className="header">SitRight</header>
       <main className="main-content">
@@ -38,7 +15,6 @@ function App() {
       </main>
     </div>
   );
->>>>>>> a7b5a24 (Nothing)
 }
 
 export default App;
