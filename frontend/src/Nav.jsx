5a4c356316448e1dbfb5959dd8e1906fe3959673
JohnPalmer21 <Jpalmer7951@gmail.com>
2025-03-07 17:08:45 -0500

    created nav bar

---
+++ frontend/src/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './nav.css';
+
+function Nav() {
+  
+    return (
+      <div className="body">
+        <div className="topnav">
+            <ul>
+                <li><a href = "">home</a></li>
+                <li><a href = "">Stats</a></li>
+                <li><a href = "">more</a></li>
+            </ul>
+
+        </div>
+    
+    
+      </div>
+
+    )
+  }
+
+export default Nav;
+  
