1e157a8ba12dbf54563248f444f486c50ca855b7
JohnPalmer21 <Jpalmer7951@gmail.com>
2025-03-07 21:29:28 -0500

    doing stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,46 @@
-import Webcam from 'react-webcam';
-import React from "react";
+import React, { useState } from "react";
+import Nav from "./nav";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+ HEAD
+  const [light, setlight] = useState(true)
+
+  const toggleTheme = () => {
+    setlight(!light);
+  };
 
   return (
-    <>
+    <div className={light ? 'light' : 'dark'}>
      
      <Nav />
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      
-    </>
+    
+     
+      <button onClick={toggleTheme}>
+      {light ? "Light" : "Dark"} Mode
+      </button>
+
+      <div>
+      <header className="header">SitRight</header>
+      <main className="main-content">
+        <p>Welcome to SitRight!</p>
+
+        {/* Camera Placeholder */}
+        <div className="camera-container">
+          <Webcam className="webcam" />
+          <canvas className="overlay-canvas"></canvas>
+        </div>
+
+        <div className="App">
+          
+        </div>
+
+      </main>
+    </div>
+    
+    </div>
   )
+
 }
 
 export default App;
