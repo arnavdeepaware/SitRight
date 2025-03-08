324ed7e97b46553a8cea0d5ee6842c6721ca3552
Aditya Dwivedi <aditya.vd2002@gmail.com>
2025-03-07 21:22:38 -0500

    Camera again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,8 +17,29 @@ function App() {
         
       </div>
       
+    
+  
+    <div>
+      <header className="header">SitRight</header>
+      <main className="main-content">
+        <p>Welcome to SitRight!</p>
+
+        {/* Camera Placeholder */}
+        <div className="camera-container">
+          <Webcam className="webcam" />
+          <canvas className="overlay-canvas"></canvas>
+          <h1>Camera</h1>
+        </div>
+
+        <div className="App">
+          
+        </div>
+
+      </main>
+    </div>
+
     </>
-  )
+    );
 }
 
 export default App;
